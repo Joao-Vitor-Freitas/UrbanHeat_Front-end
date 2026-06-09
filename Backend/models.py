1d63c6c77cb672e0a65f45366f9542b5448c62a0
Mr.JV <joaovitordutrafreitas@gmail.com>
2026-06-08 22:18:07 -0300

    Apenas o front do projeto
    trabalho

--- Backend/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from datetime import datetime
-
-@dataclass
-class Medicao:
-    sensor_id: str
-    temperatura: float
-    umidade: float
-    data_hora: datetime
